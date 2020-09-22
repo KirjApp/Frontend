ee290525f8127951e855dcab79ba2627a44b5607
Esa Mäkipää <esa.makipaa@gmail.com>
2020-09-22 18:19:40 +0300

    Uusi branch kirjanäkymän ja arvostelujen näyttämisen testaamiseen

--- src/services/data.js
+++ src/services/data.js
@@ -22,7 +22,7 @@ axios.get('https://www.google.com/search', {
 })
 */
 
-// Getting book data from Google Books API
+// kirjojen haku Google Books APIsta
 const getAll = newFilter => {
   const request = axios.get(baseUrl, {
     params: {
@@ -34,32 +34,16 @@ const getAll = newFilter => {
   return request.then(response => response.data.data.items)
 }
 
-// Saving book and/or review
+// kirjan ja/tai arvostelujen tallentaminen tietokantaan (MongoDB)
 const create = newObject => {
   const request = axios.post(myBaseUrl, newObject)
   return request.then(response => response.data)
 }
-/*
-const getAll = newFilter => {
-  //const request = axios.get(`https://www.googleapis.com/books/v1/volumes?q=${props.newFilter}t&maxResults=20&projection=full`)
-  const request = axios.get(baseUrl + `/?query=${newFilter}`)
-  return request.then(response => response.data.data.items)
-}
-*/
-/*
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
+
+// kirjan arvostelujen haku tietokannasta (MongoDB)
+const getReviews = id => {
+  const request = axios.get(myBaseUrl + '/' + id)
   return request.then(response => response.data)
 }
 
-const deleteSelected = id => {
-  const request = axios.delete(baseUrl + '/' + id)
-  return request.then(response => response.data)
-}
-
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
-*/
-export default { getAll, create /*, create, deleteSelected, update*/ }
+export default { getAll, create, getReviews /*, create, deleteSelected, update*/ }
